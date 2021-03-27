e1331317678a1088b7e27ed2e9e971abe00a79ac
za505 <za505@nyu.edu>
2021-03-27 14:00:11 -0400

    end of re-analysis of 03172021_Exp1, 03252021_Exp1&2, and 03262021

--- matlab_templates/BacTrack/BTfluoAVG.m
+++ matlab_templates/BacTrack/BTfluoAVG.m
@@ -20,11 +20,12 @@ clear, close all
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %USER INPUT
-basename=["03172021_Exp2_colony1", "03172021_Exp2_colony2","03172021_Exp2_colony3",];%Name of the image stack, used to save file.
-filename=['/Users/zarina/Downloads/NYU/Year2_2021_Spring/03172021_analysis/03172021_Exp2'];
+basename=["03252021_Exp2_colony1", "03252021_Exp2_colony2", "03252021_Exp2_colony3", "03252021_Exp2_colony4"];%Name of the image stack, used to save file.
+filename=['/Users/zarina/Downloads/NYU/Year2_2021_Spring/03252021_analysis/03252021_Exp2'];
 channel=['_647'];
-recrunch=0;
-B=length(basename); %number of main directories to analyze
+%recrunch=1;
+B=length(basename);%number of main directories to analyze
+mgGradient=1;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %load the time variables
 base=char(basename(1))
@@ -32,15 +33,25 @@ cd([filename '/' base '/' base channel '/' base '_figures'])
 filelist{1}=dir([base '_BTfluo.mat']);
 T = cell2mat(struct2cell(load([filelist{1}.name],'T')));
 time = cell2mat(struct2cell(load([filelist{1}.name],'time')));
+frameAuto = cell2mat(struct2cell(load([filelist{1}.name],'frameAuto')));
+mgConc = cell2mat(struct2cell(load([filelist{1}.name],'mgConc')));
+mgRange = cell2mat(struct2cell(load([filelist{1}.name],'mgRange')));
 
 %pre-allocate matricies
-Iout=zeros(B, T);
+%there we just want to have in one place to see
+cellAdj=zeros(B,1);
+bgAdj=zeros(B,1);
+bgIntensity=zeros(B,T);
+cellIntensity=[];
 ratio=[];
-%intensity_avg=zeros(B,T);
-intensity=[];
-bgAdj=[];
-frameAuto=[];
+Iout=zeros(B, T);
+Iin=[];
 
+%these are the ones we'll be re-calculating/plotting
+avgIin=zeros(1, T);
+stdIin=zeros(1, T);
+avgRatio=zeros(1, T);
+stdRatio=zeros(1, T);
 
 for b=1:B
     
@@ -48,41 +59,40 @@ for b=1:B
     cd([filename '/' base '/' base channel '/' base '_figures'])
     filelist{b}=dir([base '_BTfluo.mat']);
     
-    bgAdj(b) = cell2mat(struct2cell(load([filelist{b}.name],'bgAdj')));
-    frameAuto(b) = cell2mat(struct2cell(load([filelist{b}.name],'frameAuto')));
+    bgAdj(b,1) = cell2mat(struct2cell(load([filelist{b}.name],'bgAdj')));
+    cellAdj(b,1) = cell2mat(struct2cell(load([filelist{b}.name],'cellAdj')));
+    bgIntensity(b, :) = cell2mat(struct2cell(load([filelist{b}.name],'bgIntensity')));
     Iout(b, :) = cell2mat(struct2cell(load([filelist{b}.name],'Iout')));
-    ratio(b, :) = cell2mat(struct2cell(load([filelist{b}.name],'ratio')));
-    temp=struct2cell(load([filelist{b}.name],'icell_temp'));
-    %temp2=struct2cell(load([filelist{b}.name],'icellAvg_temp'));
+    temp=struct2cell(load([filelist{b}.name],'cellIntensity'));
+    ratio_temp=struct2cell(load([filelist{b}.name],'ratio'));
+    Iin_temp=struct2cell(load([filelist{b}.name],'Iin'));
     
-    %subtract background from the intensity
-    temp=cell2mat(temp{1,1});
-    temp=temp-bgAdj(b);
+    %let's get all the raw intensities, adj intensities, and ratios in one place
+    temp=temp{1,1};
+    cellIntensity=[cellIntensity; temp];
     
-    %now let's calulate the ratio
-    ratio_temp=temp./Iout(b, :)
-    %ratio_temp(:,1:frameAuto(b))=NaN;
-    
-    intensity=[intensity; temp];
+    ratio_temp=ratio_temp{1,1};
     ratio=[ratio; ratio_temp];
-    %intensity_avg(b,:)=cell2mat(temp2{1,1});
     
+    Iin_temp=Iin_temp{1,1};
+    Iin=[Iin; Iin_temp];
+  
 end
     
-%what intensity do you expect the population average to be?
-%exp_popavg = nanmean(intensity_avg);
+%let's calulcate the population average intensity based on all the cells
+avgIin = mean(Iin, 'omitnan');
+stdIin = std(Iin, 'omitnan');
 
 %remove ratios from frames without dye
-ratio(:,1:frameAuto(b))=NaN;
-
-%let's calulcate the population average intensity based on all the cells
-intensity_popavg = nanmean(intensity);
-intensity_popstd = std(intensity, 'omitnan');
+ratio(:,1:frameAuto)=NaN;
 
 %let's calulcate the population average based on all the cells
-ratio_popavg = nanmean(ratio);
-ratio_popstd = std(ratio, 'omitnan');
+avgRatio = mean(ratio, 'omitnan');
+stdRatio = std(ratio, 'omitnan');
 
+%change directory
+cd(filename);
+save(['BTfluoAVG' channel])
 
 %let's plot the population avereage intensity
 figure, hold on
@@ -91,24 +101,38 @@ xlabel('Time (s)')
 ylabel('Intensity (A.U.)')
 fig2pretty
 ylim([0 Inf])
-ciplot(intensity_popavg - intensity_popstd, intensity_popavg + intensity_popstd, time, [0.75 0.75 1])
+ciplot(avgIin - stdIin, avgIin + stdIin, time, [0.75 0.75 1])
 %plot(time, exp_popavg)
-plot(time, intensity_popavg, '-r')
-xline(90, '--k', '*PBS + 5% detergent')
-xline(210, '--k', '*PBS + 647') 
-xline(330, '--k', '*PBS + 647 + FSS') 
-xline(450, '--k', '*PBS + 647 + CF')
-xline(570, '--k', '*PBS + 647 + AF')
+plot(time, avgIin, '-r')
+xline(200, '--k', 'PBS + 647') %frame 20-32
+xline(330, '--k', 'PBS + 647 + 12 mM Mg') %frame 33-45
+xline(460, '--k', 'PBS + 647 + 15 mM Mg') %frame 46-58
+xline(590, '--k', 'PBS + 647 + 20 mM Mg') %frame 59-66
+saveas(gcf, ['avgIin' channel '.png'])
 
 %let's plot the average ratio too
 figure, hold on
 title('Intensity/Background vs Time')
 xlabel('Time (s)')
 ylabel('Intensity/Background')
+ylim([0 Inf])
 fig2pretty
-ciplot(ratio_popavg - ratio_popstd, ratio_popavg + ratio_popstd, time, [0.75 0.75 1])
-plot(time, ratio_popavg, '-r')
-xline(210, '--k', '*PBS + 647') 
-xline(330, '--k', '*PBS + 647 + FSS') 
-xline(450, '--k', '*PBS + 647 + CF')
-xline(570, '--k', '*PBS + 647 + AF')
+ciplot(avgRatio - stdRatio, avgRatio + stdRatio, time, [0.75 0.75 1])
+plot(time, avgRatio, '-r') 
+xline(200, '--k', 'PBS + 647') %frame 20-32
+xline(330, '--k', 'PBS + 647 + 12 mM Mg') %frame 33-45
+xline(460, '--k', 'PBS + 647 + 15 mM Mg') %frame 46-58
+xline(590, '--k', 'PBS + 647 + 20 mM Mg') %frame 59-66
+saveas(gcf, ['avgRatio' channel '.png'])
+
+%let's plot the average ratio vs Mg2+ 
+figure, hold on
+title('Average Intensity/Background vs Mg^{2+} Concentration')
+xlabel('Mg^{2+} (mM)')
+ylabel('Intensity/Background')
+yline(1, '--b')
+xlim([-2 22])
+xticks(mgRange)
+fig2pretty
+plot(mgConc, avgRatio, '-r') 
+saveas(gcf, ['avgRatio_mgConc' channel '.png'])
