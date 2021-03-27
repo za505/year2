b2e374b381caac3c0b1a309b395677f576578dfb
za505 <za505@nyu.edu>
2021-03-27 09:37:19 -0400

    initial run

---
+++ matlab_templates/analysis/analysisCheck.m
@@ -0,0 +1,15 @@
+%analysisCheck.m
+%Author: Zarina Akbary
+%Date: 27 March 2021
+%Purpose: go through multiple experiments and quality control the analysis
+
+%Questions
+%1Q. How many experiments are we re-analyzing?
+%1A. Three. 03172021_Exp1, 03252021_Exp1, and 03262021_Exp1
+
+%2Q. How many colonies are in each?
+%2A. There are three in the first and four in the other two
+
+%3Q. What do you want to do first?
+%3A. Verify the accuracy of the cell tracking from BacTrack.m
+
