8d3a9489f91fc76dcc2cc47a21a8559a9ed5d06c
za505 <za505@nyu.edu>
2021-05-11 19:14:31 -0400

    05082021_growthCurve

--- matlab_templates/analysis/multiGrowthCurve.m
+++ matlab_templates/analysis/multiGrowthCurve.m
@@ -12,13 +12,13 @@ clear, close all
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %INPUT
-filename=('03092021_growthCurve');
-dirname=('/Users/zarina/Downloads/NYU/Year2_2021_Spring/growthCurves/03092021/');
+filename=('05082021_growthCurve');
+dirname=('/Users/zarina/Downloads/NYU/Year2_2021_Spring/growthCurves/05082021/');
 filepath=[dirname filename '.xlsx'];
 
-xlRange='B51:EM86'; %what is the range of values for the OD data
+xlRange='B51:EM104'; %what is the range of values for the OD data
 timeRange='B49:EM49'; %what is the range of time values
-wellRange='A51:A86'; %wells are we pulling data from
+wellRange='A51:A104'; %wells are we pulling data from
 
 OD=xlsread(filepath, xlRange); %OD data
 t=xlsread(filepath, timeRange); %time data
@@ -33,14 +33,14 @@ time2=([0:T-1]*600)/3600;
 
 %%%%%First, the 7H9 data
 %Now, let's define conditions
-condlab={'7H9', 'LB', 'LB + 1 M sorbitol'};
-strainlab={'ER444', 'ER445', 'ER446'};
+condlab={'LB', 'LB + 1 M sorbitol', 'LB + 20 mM Mg^{2+}'};
+strainlab={'ER048', 'ER419', 'ER420', 'ER435'};
 
 %pre-allocate a structure
 growth = struct; 
 
 %Let's take an average of the blanks
-blank1=[28:30]; %7H9 blank
+blank1=[37:39, 46:48]; %LB blank
 blank1_avg = mean(OD(blank1, :));
 
 %flag = 0;
@@ -74,7 +74,7 @@ for j=1:length(strainlab)
 plot(tim,growth(j).eODsmooth)
 end
 xlabel('Time (h)')
-ylabel('Growth Rate in 7H9 (h^{-1})')
+ylabel('Growth Rate in LB (h^{-1})')
 fig2pretty
 legend(strainlab)
 cd(dirname)
@@ -86,7 +86,7 @@ for j=1:length(strainlab)
 plot(time2,growth(j).ODnorm)
 end
 xlabel('Time (h)')
-ylabel('OD in 7H9 (AU)')
+ylabel('OD in LB (AU)')
 fig2pretty
 legend(strainlab)
 cd(dirname)
@@ -94,9 +94,9 @@ saveas(gcf, [filename '_OD1'])
 saveas(gcf, [filename '_OD1.png'])
 
 
-%%%%%Now, the LB data
+%%%%%Now, the LB + 1 M sorbitol data
 %Let's take an average of the blanks
-blank2=[31:33]; %LB blank
+blank2=[40:42, 49:51]; %LB + 1 M sorbitol blank
 blank2_avg = mean(OD(blank2, :));
 
 %flag = 0;
@@ -130,7 +130,7 @@ for j=1:length(strainlab)
 plot(tim,growth(j+3).eODsmooth)
 end
 xlabel('Time (h)')
-ylabel('Growth Rate in LB (h^{-1})')
+ylabel('Growth Rate in LB + 1 M sorbitol (h^{-1})')
 fig2pretty
 legend(strainlab)
 cd(dirname)
@@ -142,16 +142,16 @@ for j=1:length(strainlab)
 plot(time2,growth(j+3).ODnorm)
 end
 xlabel('Time (h)')
-ylabel('OD in LB (AU)')
+ylabel('OD in LB + 1 M sorbitol (AU)')
 fig2pretty
 legend(strainlab)
 cd(dirname)
 saveas(gcf, [filename '_OD2'])
 saveas(gcf, [filename '_OD2.png'])
 
-%%%%%Now, the LB + 1 M sorbitol data
-%LB + 1 M sorbitol blank
-blank3=[34:36]; 
+%%%%%Now, the LB + 20 mM Mg^{2+} data
+%LB + 20 mM Mg^{2+} blank
+blank3=[43:45, 52:54]; 
 blank3_avg = mean(OD(blank3, :));
 
 %pre-allocate a structure
@@ -188,7 +188,7 @@ for j=1:length(strainlab)
 plot(tim,growth(j+6).eODsmooth)
 end
 xlabel('Time (h)')
-ylabel('Growth Rate in LB + 1 M sorbitol (h^{-1})')
+ylabel('Growth Rate in LB + 20 mM Mg^{2+} (h^{-1})')
 fig2pretty
 legend(strainlab)
 cd(dirname)
@@ -200,7 +200,7 @@ for j=1:length(strainlab)
 plot(time2,growth(j+6).ODnorm)
 end
 xlabel('Time (h)')
-ylabel('OD in LB + 1 M sorbitol (AU)')
+ylabel('OD in LB + 20 mM Mg^{2+} (AU)')
 fig2pretty
 legend(strainlab)
 cd(dirname)
