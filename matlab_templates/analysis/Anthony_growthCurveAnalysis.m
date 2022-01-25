b96466a26b0c0461f14ce98f529f3efa99c2b447
za505 <za505@nyu.edu>
2022-01-25 12:55:08 -0500

    01242022_growthCurve analysis

--- matlab_templates/analysis/Anthony_growthCurveAnalysis.m
+++ matlab_templates/analysis/Anthony_growthCurveAnalysis.m
@@ -2,20 +2,31 @@
 
 clear, close all
 
-filename=('12122021_growthCurve.xlsx');
-fullpath=('/Users/zarina/Downloads/NYU/Year3_2021_Fall/growthCurves');
-xlRange='B46:DY75';
-nwells=30;
-T=128;
+filename=('01242022_growthCurve.xlsx');
+fullpath=('/Users/zarina/Downloads/NYU/Year3_2022_Spring/growthCurves');
+xlRange='B50:DH103';
+nwells=54;
+T=111;
 
 %Conditions
-cond1=[1:6]; % 1:100
-cond2=[7:12]; % 1:250
-cond3=[13:18]; % 1:150
-cond4=[19:24]; % 1:1000
-blank=[25:30]; % LB blank 
+cond1=[7:12]; % Control
+cond2=[13:15]; % vanco1
+cond3=[16:18]; % vanco2
+cond4=[19:21]; % amp1
+cond5=[22:24]; % amp2 
+cond6=[25:27]; % cef1
+cond7=[28:30]; % cef2 
+cond8=[31:33]; % fos1
+cond9=[34:36]; % fos2
+cond10=[37:39]; % carb1
+cond11=[40:42]; % carb2
+cond12=[43:45]; % tun1
+cond13=[46:48]; % tun2
+cond14=[49:51]; % moe1
+cond15=[52:54]; % moe2
+blank=[1:6];
 
-condlab={'1:100','1:250', '1:500', '1:1000'};
+condlab={'Control', 'vancomycin 1', 'vancomycin 2', 'ampicillin 1', 'ampicillin 2', 'cefsulodin 1', 'cefsulodin 2', 'phosphomycin 1', 'phosphomycin 2', 'carbenicillin 1', 'carbenicillin 2', 'tunicamycin 1', 'tunicamycin 2', 'moenomycin 1', 'moenomycin 2'};
 
 %upload file
 cd(fullpath)
@@ -27,6 +38,17 @@ WellInd={cond1; %first column, cond wells, second column blanks
   cond2;
   cond3;
   cond4;
+  cond5;
+  cond6;
+  cond7;
+  cond8;
+  cond9;
+  cond10;
+  cond11;
+  cond12; 
+  cond13;
+  cond14;
+  cond15;   
   };
 
 [Ncond ~]=size(WellInd); % # rows = # conditions
@@ -70,7 +92,7 @@ time2=([0:T-2]*600)/3600;
 time3=([0:T-1]*600)/3600;
 
 
-figure
+% figure
 plot(time2,eOD_smooth*3600)
 title('Growth Rate')
 xlabel('Time (h)')
@@ -85,19 +107,44 @@ xlabel('Time (h)')
 ylabel('OD(AU)')
 legend(condlab)
 fig2pretty
-
-
-figure
-%ciplot((OD_norm_smooth-std_OD),(OD_norm_smooth+std_OD),time3,[0.75 0.75 1])
-xlabel('Time (h)')
-ylabel('OD(AU)')
-fig2pretty
-hold on
-plot(time3,OD_norm_smooth,'LineWidth',2)
-title('Growth Curve')
-xlabel('Time (h)')
-ylabel('OD(AU)')
-legend(condlab)
+% 
+% 
+% figure
+% %ciplot((OD_norm_smooth-std_OD),(OD_norm_smooth+std_OD),time3,[0.75 0.75 1])
+% xlabel('Time (h)')
+% ylabel('OD(AU)')
+% fig2pretty
+% hold on
+% plot(time3,OD_norm_smooth,'LineWidth',2)
+% title('Growth Curve')
+% xlabel('Time (h)')
+% ylabel('OD(AU)')
+% legend(condlab)
 
 %% Prediction
+% [~, idx] = max(OD_norm_smooth(1,:));
+% y=OD_norm_smooth(1, 1:idx);
+% x=time3(1, 1:idx);
+% f=fit(x',y','exp1')
+% 
+% figure(1)
+% plot(f, x,y)
+% 
+% [~, idx] = max(OD_norm_smooth(2,:));
+% y=OD_norm_smooth(2, 1:idx);
+% x=time3(1, 1:idx);
+% f=fit(x',y','exp1')
+% 
+% figure(2)
+% plot(f, x,y)
+%% Function
 
+% function [dilution_factor]=dilPredict(current_OD, incubation_time, OD_curve, time)
+% % 
+% [~, idx] = max(OD_curve);
+% y=OD_curve(1, 1:idx);
+% x=time(1, 1:idx);
+% f=fit(x',y','exp1');
+
+
+% end
